cae59f73faddd6910047361845538cd38626e84f
Akiko Ogawa <akiko.ogawaa@gmail.com>
2021-08-24 15:09:53 +0100

    Edited file

--- preprocess_folder.py
+++ preprocess_folder.py
@@ -52,23 +52,23 @@ full_label_filepath(my_dir)
 
 #%%
 full_path = os.getcwd()
+print(full_path)
 new_dir = full_path + '/plant_imgs/'
 print(new_dir)
 
 def rename(new_dir):
-    for root, dirs, files in os.walk(new_dir):
-        for full_label in full_label_list:
-            full_label = full_label[13::]
-            folder = 0
-            for folder in range(3297):
-                if not os.path.exists(new_dir):
-                    os.makedirs(os.path.dirname(root), exist_ok=True)
-                i = 0
-                for i in range(9):
-                    os.rename(root + f'{folder}/{i}.jpg', new_dir + full_label)
+    # for root, dirs, files in os.walk(new_dir):
+    for full_label in full_label_list:
+        full_label = full_label[13::]
+        i = 0
+        for folder in range(len(full_label_list)):
+            if not os.path.exists(new_dir):
+                print(new_dir)
+                os.makedirs(os.path.join(new_dir, full_label), exist_ok=True)
+            for i in range(9):
+                os.rename(new_dir + f'{folder}/{i}.jpg', new_dir + full_label)
                 folder += 1
+                i += 1
 
 # %%
 rename(new_dir)
-
-
