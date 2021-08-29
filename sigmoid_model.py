3f826ca547fd4ea7bd8ad53faf6393f4d13668cf
Akiko Ogawa <akiko.ogawaa@gmail.com>
2021-08-29 18:26:06 +0100

    Updated some code

--- sigmoid_model.py
+++ sigmoid_model.py
@@ -1,4 +1,3 @@
-#%%
 import multiprocessing
 from scipy.sparse import data
 import torch
@@ -10,7 +9,6 @@ from sklearn import datasets, preprocessing
 from sklearn.metrics import f1_score
 import matplotlib.pyplot as plt
 
-#%%
 class RHS_Dataset(torch.utils.data.Dataset):
     def __init__(self):
         super().__init__()
@@ -97,5 +95,3 @@ dataloader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True, n
 
 # Train model
 train(model, dataloader)
-
-# %%
